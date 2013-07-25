03293f29857b00abc604e1e29fd64e53d65b28ce
Enej Bajgoric <enej.bajgoric@gmail.com>
2013-07-24 19:08:41 -0700

    Revert "Update footer.php"
    This reverts commit 014003ab4d9113f73661e4547846f7b2d08305a4.

--- footer.php
+++ footer.php
@@ -1,58 +1,11 @@
 <?php
-<div class="sidebar-bottom-left"></div><!-- end #sidebar-bottom-left--><div class="sidebar-bottom-right"></div><!-- end #sidebar-bottom-right--></div>
-	</div><!-- end #content-sidebar-wrap -->
-	</div><!-- end #inner --><div id="footer-widgets" class="footer-widgets"><div class="wrap"><div class="footer-widgets-1 widget-area"><div id="linkcat-18" class="widget widget_links"><div class="widget-wrap"><h4 class="widgettitle">LINKS</h4>
-
-	<ul class='xoxo blogroll'>
-<li><a href="http://2vancouver.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://2vancouver.com']);">2Vancouver.com</a></li>
-<li><a href="http://www.protocol.gov.bc.ca/protocol/prgs/consular/directory.htm" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.protocol.gov.bc.ca']);">B.C. Consular Corps Directory</a></li>
-<li><a href="http://www.cbc.ca/bc/" title="CBC British Columbia" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.cbc.ca']);">CBC British Columbia</a></li>
-<li><a href="http://vancouver.ca/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://vancouver.ca']);">City of Vancouver</a></li>
-<li><a href="http://www.peretz-centre.org/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.peretz-centre.org']);">Peretz Centre for Secular Jewish Culture</a></li>
-<li><a href="http://www.straight.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.straight.com']);">The Georgia Straight</a></li>
-<li><a href="http://www.theglobeandmail.com/" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.theglobeandmail.com']);">The Globe and Mail</a></li>
-<li><a href="http://www.montrealgazette.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.montrealgazette.com']);">The Montreal Gazette</a></li>
-<li><a href="http://www.ricepapermagazine.ca" title=" Ricepaper Magazine &#8211; Asian Art &amp; Culture" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.ricepapermagazine.ca']);">The Ricepaper Magazine</a></li>
-<li><a href="http://thetyee.ca/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://thetyee.ca']);">The Tyee</a></li>
-<li><a href="http://www.vancourier.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.vancourier.com']);">The Vancouver Courier</a></li>
-<li><a href="http://www.vancouversun.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.vancouversun.com']);">The Vancouver Sun</a></li>
-<li><a href="http://www.walrusmagazine.com/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.walrusmagazine.com']);">The Walrus Magazine</a></li>
-<li><a href="http://www.viff.org/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.viff.org']);">Vancouver International Film Festival &amp; Centre</a></li>
-<li><a href="http://www.creativetechnology.org/" target="" onclick="javascript:_gaq.push(['_trackEvent','outbound-blogroll','http://www.creativetechnology.org']);">W2 Community Media Art</a></li>
-
-	</ul>
-</div></div>
-
-</div><div class="footer-widgets-2 widget-area"><div id="text-2" class="widget widget_text"><div class="widget-wrap"><h4 class="widgettitle">SPEAK YOUR MIND</h4>
-			<div class="textwidget"><p><br />We welcome appropriate, unsolicited editorial submissions if accompanied by the author's real name, address and telephone number. </p>
-
-<p>We also welcome your comments and suggestions, by mail or email. </p>
-
-<p>----</p>
-
-<p>For advertising please call: <br  \>
-604-682-5545</p></div>
-		</div></div>
-</div><div class="footer-widgets-3 widget-area"><div id="text-9" class="widget widget_text"><div class="widget-wrap"><h4 class="widgettitle">CONTACT US</h4>
-			<div class="textwidget"><p><br /><strong>THE SOURCE NEWSPAPER </strong></p>
-
-<p><strong>Mailing Address:</strong><br />
-Denman Place PO Box 47020,<br />
-Vancouver BC V6G 3E1</p>
-
-<p><strong>Office:</strong><br />
-204-825 Granville Street, Vancouver BC</p>
-
-<p><strong>Phone:</strong> 604-682-5545<br />
-<strong>Courriel:</strong><a href="http://thelasource.com/en/contact" > info@thelasource.com</a></p></div>
-		</div></div>
-</div></div><!-- end .wrap --></div><div id="footer" class="footer"><div class="wrap"><div id="footer" class="footer"><div class="wrap"><div class="gototop"><p><a href="#wrap" rel="nofollow">Return to top of page</a></p></div><div class="creds">
-  <p>Copyright &copy; 1999-2012 &middot;The Source Newspaper<a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework"></a> &middot;Powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a> &amp; <a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework">Genesis Framework</a></p>
-</div></div><!-- end .wrap --></div><!-- end #footer -->
-</div></div><!-- end .wrap --></div><!-- end #footer -->
-</div><!-- end #wrap -->
-	<div style="display:none">
-	</div>
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Expound
+ */
 ?>
 
 	</div><!-- #main -->
