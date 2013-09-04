f2d525787164507cd0faaa5b27f556bca6876f9d
luizalibardi <luizalibardi@Luizas-MacBook-Pro.local>
2013-09-03 19:43:13 -0700

    restore cover article

--- header.php
+++ header.php
@@ -13,23 +13,14 @@
 
 global $edition, $edition_link;
 
-if( is_tax( 'edition' ) ):
-	// $term_idz = get_queried_object_id();
-else:
-	$term_idz = get_theme_mod('home_edition');
-endif;
+$edition = get_term_by( 'id', get_theme_mod('home_edition'),'edition');
+$volume  = get_term_by( 'id', $edition->parent, 'edition');
 
-	$edition = get_term_by( 'id', $term_idz, 'edition' );
-	$volume  = get_term_by( 'id', $edition->parent, 'edition' );
-	$edition_link = get_term_link( $edition );
-	$edition_name = ( is_object($volume) ? $volume->name.", ".$edition->name." - ".$edition->description : $edition->name." - ".$edition->description ); 
-
-
-
+$edition_link = get_term_link( $edition );
+$edition_name = ( is_object($volume) ? $volume->name.", ".$edition->name." - ".$edition->description : $edition->name." - ".$edition->description ); 
 // $edition->name
 // $edition->description
-// $edition->slug
-// $edition->term_id		
+// $edition->slug		
 		?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -51,45 +42,18 @@ endif;
 	<img src="/wp-content/uploads/2013/08/source_icon_white.png"/>
 	<span class="topbar-language"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">ENGLISH</a></span> &middot; <span class="topbar-edition"><a class='expand-archive'><?php echo $edition_name; ?></a></span>
 <form method="post" class='archive'>
-            	Select an issue: 
-            	<?php
-            	
-            	class Walker_SlugValueCategoryDropdown extends Walker_CategoryDropdown {
-            		function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
-						$pad = str_repeat('&nbsp;', $depth * 3);
-
-						$cat_name = apply_filters('list_cats', $category->name, $category);
-						$output .= "\t<option class=\"level-$depth\" value=\"".$category->slug."\"";
-					
-						if ( $category->term_id == $args['selected'] )
-							$output .= ' selected="selected"';
-					
-						$output .= '>';
-						$output .= $pad.$cat_name;
-						if ( $args['show_count'] )
-							$output .= '&nbsp;&nbsp;('. $category->count .')';
-						$output .= "</option>\n";
-					}
-				}
-
-
-            	$volume_args = array(
-            		'name'=>'ed',
-            		'taxonomy'=>'edition',
-            		'hierarchical'=>true,
-            		'walker'=>new Walker_SlugValueCategoryDropdown);
-
-            	wp_dropdown_categories($volume_args);
-            	?>
-            	<script type="text/javascript"><!--
-    				var dropdown = document.getElementById("ed");
-    				function onEditionChange() {
-						if ( dropdown.options[dropdown.selectedIndex].value) {
-							location.href =  "<?php echo get_option('home');?>/?edition="+dropdown.options[dropdown.selectedIndex].value;
-						}
-    				}
-    				dropdown.onchange = onEditionChange;
-			--></script>
+            	Volume: 
+            	<select name="volume">
+	                <option value="2012">2012</option>
+	                <option value="2011">2011</option>
+            	</select>
+            	Issue:  
+	            <select name="issue">
+	                <option value="July">July</option>
+	                <option value="June">June</option>
+	                <option value="May">May</option>
+	            </select>
+            <input id='archive_submit' type="submit" value="Get issue">
         	</form>
 </div>
 	<div id="topbar-right" class="topbar-right">retrouvez-nous:
@@ -121,8 +85,7 @@ endif;
                         <?php if ( !dynamic_sidebar( 'header-widget-area' ) )?>
            </div>
 <?php endif; ?>
-		</div>
-		
+		</div>		
 
 		<nav id="site-navigation" class="navigation-main" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'expound' ); ?></h1>
@@ -132,4 +95,4 @@ endif;
 			<?php do_action( 'expound_navigation_after' ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-	<div id="main" class="site-main">
+    <div id="main" class="site-main">
