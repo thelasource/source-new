a61d3c23689fc8ebcc0ea2285aafccacb6cea5bc
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2014-01-19 11:59:04 -0800

    shortcode for generating QR codes

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -1,6 +1,7 @@
 <?php 
 
 add_shortcode('shortlinks', 'source_shortlinks');
+add_shortcode('generate_QRcodes', 'source_QRcodes');
 
 function source_shortlinks($atts){
 	// Query the current edition
@@ -25,4 +26,36 @@ function source_shortlinks($atts){
 	wp_reset_postdata();
 
 	return $html;
+}
+
+function source_QRcodes(){
+	
+	
+		// Generate form to create QR codes
+		$html  = "<form action='".get_permalink()."' method='GET'>";
+		$html .= "<input type='hidden' name='p' value='".esc_attr(get_the_ID())."'>";
+		
+		$html .= "Volume <input type='text' name='_vol' value='".esc_attr($_GET['_vol'])."'><br />";
+		$html .= "Edition <input type='text' name='_ed' value='".esc_attr($_GET['_ed'])."'><br />";
+		$html .= "Num. articles <input type='text' name='_size' value='".esc_attr($_GET['_size'])."'><br />";
+		$html .= "<input type='submit' value='generate'>";
+		$html .= "</form>";
+		
+
+		if($_GET['_size']){
+			$url = site_url()."?_volume=".$_GET['_vol']."&_edition=".$_GET['_ed'];
+			$html .= "<div style=''><img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".urlencode($url)."&choe=UTF-8 />";
+			$html .= "QR code for volume ".$_GET['_vol'].", edition ".$_GET['_ed'].":<br/>".$url."</div>";
+			for($i = 1; $i<=(int)$_GET['_size']; $i++){
+				$url = site_url()."?_volume=".$_GET['_vol']."&_edition=".$_GET['_ed']."&_article=".$i;	
+				$html .= "<div style=''><img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".urlencode($url)."&choe=UTF-8 />";
+				$html .= "QR code for volume ".$_GET['_vol'].", edition ".$_GET['_ed'].", article ".$i.":<br/>".$url."</div>";
+			}
+			
+		}
+		return $html;
+}
+
+function source_generateQRcode(){
+
 }
