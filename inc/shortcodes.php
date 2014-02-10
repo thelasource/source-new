373138dd35739c08ffa3d7af053311fd0078313b
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2014-02-09 21:36:22 -0800

    new format for QR codes - volume/edition/category

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -43,28 +43,25 @@ function source_shortlinks($atts){
  */
 function source_QRcodes(){
 	
-	
+		$categories = array('cover story' => 'cover', 'verbatim' => 'verbatim');
+
 		// Generate form to create QR codes
 		$html  = "<form action='".get_permalink()."' method='GET'>";
 		$html .= "<input type='hidden' name='p' value='".esc_attr(get_the_ID())."'>";
 		
 		$html .= "Volume <input type='text' name='_vol' value='".esc_attr($_GET['_vol'])."'><br />";
 		$html .= "Edition <input type='text' name='_ed' value='".esc_attr($_GET['_ed'])."'><br />";
-		$html .= "Num. articles <input type='text' name='_size' value='".esc_attr($_GET['_size'])."'><br />";
 		$html .= "<input type='submit' value='generate'>";
 		$html .= "</form>";
 		
 
-		if($_GET['_size']){
-			$url = site_url()."?_volume=".$_GET['_vol']."&_edition=".$_GET['_ed'];
-			$html .= "<div style=''><img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".urlencode($url)."&choe=UTF-8 />";
-			$html .= "QR code for volume ".$_GET['_vol'].", edition ".$_GET['_ed'].":<br/>".$url."</div>";
-			for($i = 1; $i<=(int)$_GET['_size']; $i++){
-				$url = site_url()."?_volume=".$_GET['_vol']."&_edition=".$_GET['_ed']."&_article=".$i;	
-				$html .= "<div style=''><img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".urlencode($url)."&choe=UTF-8 />";
-				$html .= "QR code for volume ".$_GET['_vol'].", edition ".$_GET['_ed'].", article ".$i.":<br/>".$url."</div>";
-			}
-			
+		if($_GET['_vol'] && $_GET['_ed']){
+			foreach( $categories as $key => $value){
+				$url = site_url()."/QR/".$_GET['_vol']."/".$_GET['_ed']."/".$value;
+				$html .= "<div style=''><img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".
+				urlencode($url)."&choe=UTF-8 />";
+				$html .= "QR code of ".$key." for volume ".$_GET['_vol'].", edition ".$_GET['_ed'].":<br/>".$url."</div>";
+			}			
 		}
 		return $html;
 }
