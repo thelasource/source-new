afdfa34e7952cc74931f7e5922d87519cd89b156
Enej Bajgoric <enej.bajgoric@gmail.com>
2014-03-24 19:46:06 -0700

    Bug fix for header download link

--- header.php
+++ header.php
@@ -38,8 +38,8 @@
 <div id="top-bar" class="topbar">
 	<div id="topbar-content">
 	<div id="topbar-left" class="topbar-left">
-		<a href="<?php echo get_site_url(); ?>" title=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/brand/source_icon_white.png"/></a>
-		<span class="topbar-language"><a href="/<?php _e('fr','new-source'); ?>"><?php _e( 'Français','new-source');?></a></span> &middot; <span class="topbar-edition"><a class='expand-archive'><?php echo $edition_name; ?></a> &middot; <a class="pdf-download" href=<?php echo new_source_get_pdf( __( 'English', 'new-source' )  ) ?> ><i class="icon-download"></i> <?php _e('Download PDF','new-source');?> </a></span>
+		<a href="<?php site_url();?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/brand/source_icon_white.png"/></a>
+		<span class="topbar-language"><a href="/<?php _e('fr','new-source'); ?>"><?php _e( 'Français','new-source');?></a></span> &middot; <span class="topbar-edition"><a class='expand-archive'><?php echo $edition_name; ?></a> &middot; <a class="pdf-download" href=<?php echo new_source_get_pdf( __( 'English_lowres', 'new-source' )  ) ?> ><i class="icon-download"></i> <?php _e('Download PDF','new-source');?> </a></span>
 			
 			<form method="post" class='archive'>
             	Select an issue: 
